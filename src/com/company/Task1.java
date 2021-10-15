385dba167048a39e37eac3c51e6810c6cc59b93a
nkkkta <83763184+nkkkta@users.noreply.github.com>
2021-10-15 19:28:35 +0300

    1st task

---
+++ src/com/company/Task1.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+//Найдите значение функции z = ((a - 3) * b / 2) + c
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.print("A is equals: ");
+        double a = console.nextDouble();
+        System.out.print("B is equals: ");
+        double b = console.nextDouble();
+        System.out.print("C is equals: ");
+        double c = console.nextDouble();
+        double FofX = ((a - 3 * b/2) + c);
+        System.out.println("Vector of variables : " + "{ " + a + " " + b + " " + c + " " + "}");
+        System.out.println("Function: z = ((a - 3) * b / 2) + c" );
+        System.out.println("Result: " + ((a - 3) * b/2) + c);
+
+    }
+}
